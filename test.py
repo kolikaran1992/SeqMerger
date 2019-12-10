cd207004c32d2a6f8775d0ceec5fa919e0c888b4
AIMLTeam <kolikaran1992@gmail.com>
2019-12-10 17:39:07 +0530

    add insertion position

--- test.py
+++ test.py
@@ -14,14 +14,8 @@ pairs = [
     (
         'We want the power',
         'I have the power'
-    ),
-    (
-        """7 year old victim discovered strangled in bed; victim\u2019s mother also found deceased in residence with head trauma, and the suspect, victim\u2019s stepfather, found hanging from a stairway""",
-        """7-year-old victim discovered strangled in bed; victim\u2019s mother also found deceased in residence with head trauma, and the suspect, victim\u2019s stepfather, found hanging from a stairway"""
     )
 ]
 for s1, s2 in pairs[-1:]:
     a = seq_merge.extract(s1, s2)
     print(a)
-
-print("""7 year old victim discovered strangled in bed; victim\u2019s mother also found deceased in residence with head trauma, and the suspect, victim\u2019s stepfather, found hanging from a stairway"""[8:])
